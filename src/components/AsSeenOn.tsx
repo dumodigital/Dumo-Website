b6fb06bfadd91f5ceb11ac478007a4a794b23ccb
Charlie Dumo <charliedumo@Charlies-MacBook-Air-110.local>
2025-05-26 15:43:37 -0400

    Replace third logo with keenan.png in As Seen On section

--- src/components/AsSeenOn.tsx
+++ src/components/AsSeenOn.tsx
@@ -13,7 +13,7 @@ const AsSeenOn: React.FC = () => {
     {
       name: "Ohio State University",
       width: "w-40 sm:w-48",
-      image: "/images/osu.png",
+      image: "/images/keenan.png",
     },
   ];
 
