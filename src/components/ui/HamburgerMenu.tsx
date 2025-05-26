b435c873e5f9674a5468b8870995f3ec9cafa9a1
Charlie Dumo <charliedumo@Charlies-MacBook-Air-110.local>
2025-05-26 16:49:45 -0400

    Move keenan-logo.png further down and increase size for better visual appeal

--- src/components/ui/HamburgerMenu.tsx
+++ src/components/ui/HamburgerMenu.tsx
@@ -81,9 +81,9 @@ const HamburgerMenu: React.FC = () => {
           ))}
         </div>
         {/* Centered Keenan Logo */}
-        <div className="flex justify-center items-center pb-12">
+        <div className="flex justify-center items-center pb-16">
           <a href="https://keenan.osu.edu/" target="_blank" rel="noopener noreferrer">
-            <img src="/images/keenan-logo.png" alt="Keenan Logo" className="h-24 w-auto object-contain" style={{ maxWidth: 150 }} />
+            <img src="/images/keenan-logo.png" alt="Keenan Logo" className="h-32 w-auto object-contain" style={{ maxWidth: 200 }} />
           </a>
         </div>
         {/* Bottom Section: Social & Contact */}
